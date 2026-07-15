964e9a609a51bd51c825fedc7738fa561395879e
Václav Jež <jez.vasek@gmail.com>
2026-07-15 15:22:01 +0200

    add win compilation script, add extra prebuild llama.cpp wheel index to cli pyproject

---
+++ scripts/compile_cli_windows.ps1
@@ -0,0 +1,45 @@
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest
+
+$OUTPUT_DIR = "dist"
+$null = New-Item -ItemType Directory -Force -Path $OUTPUT_DIR
+
+$tempParent = [System.IO.Path]::GetTempPath()
+$BUILD_DIR = Join-Path $tempParent ([System.IO.Path]::GetRandomFileName())
+$null = New-Item -ItemType Directory -Path $BUILD_DIR
+
+try {
+    uv venv "$BUILD_DIR/.venv" --no-config --python 3.12
+    
+    . (Join-Path $BUILD_DIR ".venv/Scripts/Activate.ps1")
+
+    uv export --package pydoctor-cli --no-hashes --no-editable --output-file "$BUILD_DIR/requirements.txt"
+
+    uv pip install "Nuitka[onefile]"
+
+    uv pip install -r "$BUILD_DIR/requirements.txt" --extra-index-url https://abetlen.github.io/llama-cpp-python/whl/cpu
+
+    uv pip install --no-deps ./packages/pydoctor_shared_cst
+    uv pip install --no-deps ./packages/pydoctor_cli
+
+
+    export CCFLAGS="-O1"
+
+    python -m nuitka `
+        --standalone `
+        --onefile `
+        --lto=no `
+        --mingw64 `
+        --jobs=1 `
+        --low-memory `
+        --enable-plugin=anti-bloat `
+        --output-dir="$OUTPUT_DIR" `
+        --report=report.xml `
+        packages/pydoctor_cli/src/pydoctor_cli/cli.py
+
+} finally {
+    Write-Host "Cleaning up temporary build directory..."
+    if (Test-Path $BUILD_DIR) {
+        Remove-Item -Recurse -Force $BUILD_DIR
+    }
+}
